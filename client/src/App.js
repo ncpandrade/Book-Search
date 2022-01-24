6fb3600f1984330adca0d346e731379463d2956c
Nadine <nadine_paez@hotmail.com>
2022-01-24 11:03:51 -0500

    create heroku

--- client/src/App.js
+++ client/src/App.js
@@ -38,6 +38,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
+  // link: authLink.concat(httpLink),
   link: from ([errorLink, authLink.concat(httpLink)]),
   cache: new InMemoryCache(),
   
