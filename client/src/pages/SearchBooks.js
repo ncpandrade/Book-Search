8bd60f8d02c882b19261d36fe13d290585722022
Nadine <nadine_paez@hotmail.com>
2022-01-23 20:47:12 -0500

    saved books fix

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -61,7 +61,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    console.log(bookToSave);
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -70,11 +70,17 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await 
+      saveBook({
+        variables: {
+          input: bookToSave,
+        },
+      });
+      console.log(response);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
